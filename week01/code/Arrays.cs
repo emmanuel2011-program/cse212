1277e5a353795bcdf4fe04c3d30fe58f2a089965
emmanuel2011 <okoyee61@gmail.com>
2025-07-12 18:03:55 +0100

    added item

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -38,20 +38,11 @@ public static class Arrays
         
     }
 
-
-
-
-    /// <summary>
-    /// This function will produce an array of size 'length' starting with 'number' followed by multiples of 'number'.  For 
-    /// example, MultiplesOf(7, 5) will result in: {7, 14, 21, 28, 35}.  Assume that length is a positive
-    /// integer greater than 0.
-    /// </summary>
-    /// <returns>array of doubles that are the multiples of the supplied number</returns>
     ///<sumamry>
     /// i created the result array
     /// used the loop to determine the start and the end of the array result created
     /// i filled each index with the next multiple
-    /// i then return the filled array
+    /// i then return the filled array  which is result
     /// </summary>
     public static double[] MultiplesOf(double number, int length)
     {
@@ -62,21 +53,10 @@ public static class Arrays
         }
         return result;
 
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
-
-        // replace this return statement with your own
+       
     }
 
-    /// <summary>
-    /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
-    /// List<int>{1, 2, 3, 4, 5, 6, 7, 8, 9} and an amount is 3 then the list after the function runs should be 
-    /// List<int>{7, 8, 9, 1, 2, 3, 4, 5, 6}.  The value of amount will be in the range of 1 to data.Count, inclusive.
-    ///
-    /// Because a list is dynamic, this function will modify the existing data list rather than returning a new list.
-    /// </summary>
+    
     
     ///<summary>
     /// created variable count
